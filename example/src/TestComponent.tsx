cf48bcc74b5e22cb4ed6569fc7dd34ad5939741f
Nick Pleis <npleis+githubnew@gmail.com>
2023-03-20 13:07:56 -0700

    build: fixed up package.json to properly include common sdk

--- example/src/TestComponent.tsx
+++ example/src/TestComponent.tsx
@@ -4,18 +4,15 @@ import { Text } from 'react-native';
 import { getInstance } from '@eppo/react-native-sdk';
 
 export default function TestComponent(): JSX.Element {
-  console.log('RENDERING TEST COMPONENT');
   const assignedVariation = React.useMemo(() => {
     const eppoClient = getInstance();
-    return eppoClient.getAssignment("user-id-1", "nicks-react-native");
-  }, [])
+    return eppoClient.getAssignment('user-id-1', 'nicks-react-native');
+  }, []);
 
   var text = 'In control';
-  if(assignedVariation === 'ShowLogo') {
+  if (assignedVariation === 'ShowLogo') {
     text = 'Assigned to logo group';
   }
 
-  return (
-    <Text>{ text }</Text>
-  );
+  return <Text>{text}</Text>;
 }
