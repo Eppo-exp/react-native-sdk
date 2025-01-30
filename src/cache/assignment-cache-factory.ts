7da0a240e8fb65b41d91017eec216abe7d76ca3d
Sameeran Kunche <skunche@gmail.com>
2025-01-29 16:14:06 -0800

    Confirm functionality of hybrid assignment cache

--- src/cache/assignment-cache-factory.ts
+++ src/cache/assignment-cache-factory.ts
@@ -1,27 +1,20 @@
-// import { AssignmentCache } from '@eppo/js-client-sdk-common';
+import type { AssignmentCache } from '@eppo/js-client-sdk-common';
 
-// import { hasWindowLocalStorage } from '../configuration-factory';
+import SimpleAssignmentCache from './simple-assignment-cache';
+import HybridAssignmentCache from './hybrid-assignment-cache';
+import { AsyncStorageAssignmentCache } from './async-storage-assignment-cache';
 
-// import SimpleAssignmentCache from './simple-assignment-cache';
-// import HybridAssignmentCache from './hybrid-assignment-cache';
-// import { AsyncStorageAssignmentCache } from './async-storage-assignment-cache';
-
-// export function assignmentCacheFactory({
-//   forceMemoryOnly = false,
-//   storageKeySuffix,
-// }: {
-//   forceMemoryOnly?: boolean;
-//   storageKeySuffix: string;
-// }): AssignmentCache {
-//   const simpleCache = new SimpleAssignmentCache();
-
-//   if (forceMemoryOnly) {
-//     return simpleCache;
-//   }
-//   if (hasWindowLocalStorage()) {
-//     const localStorageCache = new AsyncStorageAssignmentCache(storageKeySuffix);
-//     return new HybridAssignmentCache(simpleCache, localStorageCache);
-//   } else {
-//     return simpleCache;
-//   }
-// }
+export function assignmentCacheFactory({
+  forceMemoryOnly = false,
+  storageKeySuffix,
+}: {
+  forceMemoryOnly?: boolean;
+  storageKeySuffix: string;
+}): AssignmentCache {
+  const simpleCache = new SimpleAssignmentCache();
+  if (forceMemoryOnly) {
+    return simpleCache;
+  }
+  const asyncStorageCache = new AsyncStorageAssignmentCache(storageKeySuffix);
+  return new HybridAssignmentCache(simpleCache, asyncStorageCache);
+}
--- src/cache/async-storage-assignment-cache.ts
+++ src/cache/async-storage-assignment-cache.ts
@@ -1,28 +1,28 @@
-// import { AbstractAssignmentCache } from '@eppo/js-client-sdk-common';
+import { AbstractAssignmentCache } from '@eppo/js-client-sdk-common';
 
-// import {
-//   BulkReadAssignmentCache,
-//   BulkWriteAssignmentCache,
-// } from './hybrid-assignment-cache';
-// import { AsyncStorageAssignmentShim } from './async-storage-assignment-shim';
+import type {
+  BulkReadAssignmentCache,
+  BulkWriteAssignmentCache,
+} from './hybrid-assignment-cache';
+import { AsyncStorageAssignmentShim } from './async-storage-assignment-shim';
 
-// export class AsyncStorageAssignmentCache
-//   extends AbstractAssignmentCache<AsyncStorageAssignmentShim>
-//   implements BulkReadAssignmentCache, BulkWriteAssignmentCache
-// {
-//   constructor(storageKeySuffix: string) {
-//     super(new AsyncStorageAssignmentShim(storageKeySuffix));
-//   }
+export class AsyncStorageAssignmentCache
+  extends AbstractAssignmentCache<AsyncStorageAssignmentShim>
+  implements BulkReadAssignmentCache, BulkWriteAssignmentCache
+{
+  constructor(storageKeySuffix: string) {
+    super(new AsyncStorageAssignmentShim(storageKeySuffix));
+  }
 
-//   setEntries(entries: [string, string][]): void {
-//     entries.forEach(([key, value]) => {
-//       if (key && value) {
-//         this.delegate.set(key, value);
-//       }
-//     });
-//   }
+  setEntries(entries: [string, string][]): void {
+    entries.forEach(([key, value]) => {
+      if (key && value) {
+        this.delegate.set(key, value);
+      }
+    });
+  }
 
-//   getEntries(): Promise<[string, string][]> {
-//     return Promise.resolve(Array.from(this.entries()));
-//   }
-// }
+  getEntries(): Promise<[string, string][]> {
+    return Promise.resolve(Array.from(this.entries()));
+  }
+}
